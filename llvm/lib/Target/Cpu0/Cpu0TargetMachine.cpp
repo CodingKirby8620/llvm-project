15a8ef8c32afef8a99ad702c79d8ec20f43c6d43
CodingKirby8620 <1287429949@qq.com>
2025-05-14 19:18:45 +0800

    Minimum Skeleton for Cpu1

---
+++ llvm/lib/Target/Cpu0/Cpu0TargetMachine.cpp
@@ -0,0 +1,11 @@
+//
+// Created by ye on 2025/5/14.
+//
+
+#include "llvm/MC/TargetRegistry.h"
+
+#define DEBUG_TYPE "Cpu0"
+
+extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeCpu0Target(){
+
+}
---
+++ llvm/lib/Target/Cpu0/TargetInfo/Cpu0TargetInfo.cpp
@@ -0,0 +1,8 @@
+//
+// Created by ye on 2025/5/14.
+//
+#include "llvm/MC/TargetRegistry.h"
+
+extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeCpu0TargetInfo(){
+
+}
